1cc84d42f3724b7e8eb3d268da3cd1ecde8eef5a
ramikonduru <88052052+ramikonduru@users.noreply.github.com>
2024-04-10 14:30:11 -0700

    Commented code removed

--- src/app/helpers/countries.service.ts
+++ src/app/helpers/countries.service.ts
@@ -5,8 +5,6 @@ import { GraphqlHelperService } from "./graphql-helper.service";
 import { Injectable } from "@angular/core";
 
 @Injectable({
-    // declares that this service should be created
-    // by the root application injector.
     providedIn: 'root',
   })
 
